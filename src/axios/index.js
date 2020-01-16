1fe099990a7d45e386bf73e77f368703d0636ef6
zhouzh <363420113@qq.com>
2020-01-16 13:50:34 +0800

    提交

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 //添加一个请求拦截器
 axios.interceptors.request.use(function (config) {
+    if(process.env.API){
+        config.url = config.url + process.env.API
+    }
+    console.log(config);
     const {method} = config;
     if(sessionStorage.getItem('Authorization')){
         config.headers['Authorization'] = sessionStorage.getItem('Authorization');
